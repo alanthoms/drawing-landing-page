16578bde57b8b5177eb8f1c50772e7b21cba951e
Alan Thomas <alanthomas171@gmail.com>
2026-03-18 19:02:16 +0000

    feature/ Add more styling to landing page

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -2,6 +2,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { useGSAP } from "@gsap/react";
 import { SplitText } from "gsap/all";
 import gsap from "gsap";
+import LiquidEther from "./LiquidEther";
 const Landing = () => {
   useGSAP(() => {
     const heroSplit = new SplitText(".title", { type: "chars, words" });
@@ -40,43 +41,50 @@ const Landing = () => {
   return (
     <div className="py-10">
       <div>
-        <section className="relative z-10 min-h-dvh w-full border border-transparent ">
-          <div className="noisy fixed inset-0 z-[100] pointer-events-none opacity-[0.03]" />
+        <section className="absolute inset-0 w-full h-full">
           <h1
             id="title-text"
-            className="title md:mt-32 mt-40 text-2xl md:text-[13vw] leading-none text-center text-green-600 opacity-[65] tracking-tight"
+            className="title absolute inset-x-0 top-1/2 -translate-y-1/2 z-[200] text-2xl md:text-[13vw] leading-none text-center text-white tracking-tight pointer-events-none"
           >
-            Daily Doodle
+            Doodlog
           </h1>
+          <LiquidEther
+            colors={["#022C22", "#00FF66", "#34D399"]}
+            mouseForce={20}
+            cursorSize={100}
+            isViscous
+            viscous={30}
+            iterationsViscous={32}
+            iterationsPoisson={32}
+            resolution={0.5}
+            isBounce={false}
+            autoDemo
+            autoSpeed={0.5}
+            autoIntensity={2.2}
+            takeoverDuration={0.25}
+            autoResumeDelay={3000}
+            autoRampDuration={0.6}
+            color0="#022C22"
+            color1="#00FF66"
+            color2="#34D399"
+          />
 
           <div className="container mx-auto absolute left-1/2 -translate-x-1/2 lg:bottom-20 top-auto md:top-[30vh] flex justify-between items-end px-5">
             <div className="flex lg:flex-row flex-col w-full gap-10 justify-between items-center lg:items-end mx-auto;">
-              <div className="space-y-5 hidden md:block text-green-100">
-                <p> Quiz for Car Enthusiasts</p>
+              <div className="space-y-5 hidden md:block text-green-200">
+                <p> Sketch. Share. Improve.</p>
                 <p className="subtitle text-3xl -mb-2 text-green-500">
-                  Show Off Your Knowledge
+                  Build your creative streak
                 </p>
               </div>
               <div>
                 <p className="subtitle text-3xl -mb-2 text-green-300">
-                  Idk whatever
+                  Make it a habit
                 </p>
               </div>
             </div>
           </div>
         </section>
-        <section className="relative z-10  w-full border  border-transparent items:center min-h-screen flex items-center justify-center">
-          <div className="flex flex-col sm:flex-row gap-4 justify-center h:200px">
-            <button className="px-20 py-8 text-2xl md:text-3xl  bg-green-600 text-white font-bold transition-all duration-300 hover:shadow-[0_0_40px_rgba(38,238,38,0.8)] hover:box-transition-all uppercase tracking-tighter">
-              <Link to="/">Login</Link>
-            </button>
-            <button className="px-20 py-8 text-2xl md:text-3xl border text-gray-500 bg-white border-white/20 transition-all duration-300 font-bold  hover:shadow-[0_0_40px_rgba(255,255,255,0.5)] hover:box-transition-all uppercase tracking-tighter">
-              <Link to="/">Sign Up</Link>
-            </button>
-          </div>
-        </section>
-
-        <div className="bg-red-600 h-5 w-5"></div>
       </div>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 const Navbar = () => {
   return (
     <div>
-      <nav className="bg-neutral-primary fixed w-full z-20 top-0 inset-s-0 border-b border-default">
+      <nav className="bg-neutral-primary fixed w-full z-20 top-0 inset-s-0 border-b border-default text-white">
         <Link to="/">Home</Link>
         <Link to="/profile"> Courses</Link>
         <Link to="/login">Login</Link>
